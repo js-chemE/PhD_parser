ef38fa3116b4286844aeab9bfdaa4c62c9d78f3e
Julius Sommer <julius@hof-sommer.org>
2026-03-30 00:51:37 +0200

    updated raman parser

--- src/phd_parser/raman/core.py
+++ src/phd_parser/raman/core.py
@@ -1,12 +1,25 @@
-from pydantic import BaseModel, Field
-import pandas as pd
+#from pathlib import Path
 
-from typing import Any, Dict
+from pydantic import BaseModel
+#import pandas as pd
+# import ramanspy as rp
+#from phd_parser.raman import btc655n
+
+#from typing import Any, Dict
 
 class RamanData(BaseModel):
     class Config:
         arbitrary_types_allowed = True
         validate_assignment = True
 
-    data: pd.DataFrame = Field(..., description="DataFrame containing the Raman data")
-    meta: Dict[str, Any] = Field(..., description="Dictionary containing the metadata")
+
+    # @classmethod
+    # def from_btc655n_export(cls, file_path: str | Path) -> "RamanData":
+    #     # Read the data using btc655n module
+    #     raw_data = btc655n.read_export(file_path)
+
+    #     # Extract the data and metadata
+    #     data = raw_data.data
+    #     meta = raw_data.meta
+
+    #     return cls(data=data, meta=meta)
